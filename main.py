c6f1b59b8a0732c23587f3dbc48f721d6dab6c6c
yujin01-s <snam6879@naver.com>
2025-05-27 14:03:03 +0900

    메인 계산기 추가

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print("Hello")
+from add import add 
+from subtract import subtract 
+from multiply import multiply 
+from divide import divide 
+ 
+def calculator(): 
+    a = float(input("첫 번째 숫자: ")) 
+    b = float(input("두 번째 숫자: ")) 
+    op = input("연산자 (+, -, *, /): ") 
+    operations = { 
+        '+': add, 
+        '-': subtract, 
+        '*': multiply, 
+        '/': divide 
+    } 
+    if op in operations: 
+        try: 
+            result = operations[op](a, b) 
+            print("결과:", result) 
+        except Exception as e: 
+            print("오류:", e) 
+    else: 
+        print("올바르지 않은 연산자입니다.") 
+ 
+if __name__ == "__main__": 
+    calculator()
